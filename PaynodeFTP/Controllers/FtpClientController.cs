3c44816aeb47244e89e2ca64ccda90c66cfc3744
Compilenix <compilenix@gmail.com>
2018-12-30 12:54:17 +0100

    add FtpClientController

---
+++ PaynodeFTP/Controllers/FtpClientController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaynodeFTP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FtpClientController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IList<string>> Index()
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://151.217.46.28/mirror/iso/latest");
+            request.Method = WebRequestMethods.Ftp.ListDirectory;
+
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential ("anonymous","foo@bar.net");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            var result = new List<string>(reader.ReadToEnd().Split("\r\n"));
+            result.RemoveAll(x => x.Length == 0);
+
+            reader.Close();
+            response.Close();
+
+            return result;
+        }
+
+        // [HttpGet("{id}")]
+        // public ActionResult<string> Get(int id)
+        // {
+        //     return "value";
+        // }
+    }
+}
